f0008037be8fbe094e8d60cb5538cc6d716b68fc
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2026-09-17 11:06:11 +0530

    add changes

--- vite.config.ts
+++ vite.config.ts
@@ -13,13 +13,13 @@ export default defineConfig({
     },
   },
 
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:8000/api',
-        changeOrigin:true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  // server:{
+  //   proxy:{
+  //     '/api':{
+  //       target:'http://localhost:8000/api',
+  //       changeOrigin:true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // }
 })
